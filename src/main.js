4a8da24eb9ad27c6acc62097a5906a6490ea6a04
FuQunYing <fqylovae@outlook.com>
2018-10-31 15:02:40 +0800

    init commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuex from 'vuex'
+import Antd from 'ant-design-vue'
+import ViserVue from 'viser-vue'
+import 'ant-design-vue/dist/antd.css'
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.use(Antd);
+Vue.use(ViserVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/router/Dashboard/Analysis.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>
+    数据可视化
+  </p>
+</template>
+
+<script>
+  export default {
+    name: "Analysis"
+  }
+</script>
+
+<style scoped>
+
+</style>
